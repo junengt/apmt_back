1e0ae85f9c93e37937247df7751c1c726dc81c1c
koohanmo <95681583+dubukkami@users.noreply.github.com>
2022-04-21 19:39:17 +0900

    sellList++

--- src/main/java/click/applemt/apmt/repository/userrepository/userRepository.java
+++ src/main/java/click/applemt/apmt/repository/userrepository/userRepository.java
@@ -4,7 +4,7 @@ import click.applemt.apmt.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+
 import java.util.Optional;
 
 @Repository
