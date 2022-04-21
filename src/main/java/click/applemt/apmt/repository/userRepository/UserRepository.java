f8be0ca4a679bf6f8bcd96feb6e39648d4e5d39e
junengt <95681705+junengt@users.noreply.github.com>
2022-04-21 23:41:56 +0900

    9.. 상품 조회 쿼리 작성중

--- src/main/java/click/applemt/apmt/repository/userRepository/UserRepository.java
+++ src/main/java/click/applemt/apmt/repository/userRepository/UserRepository.java
@@ -1,10 +1,8 @@
-package click.applemt.apmt.repository.userrepository;
+package click.applemt.apmt.repository.userRepository;
 
 import click.applemt.apmt.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface userRepository extends JpaRepository<User, Long> {
