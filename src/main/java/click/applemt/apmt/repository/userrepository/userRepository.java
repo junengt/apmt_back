9d1b5545d0b604d8c99d726f7b1beccfe3410f22
CokeBear <43844779+Kangchanghwan@users.noreply.github.com>
2022-04-24 02:08:49 +0900

    ++

--- src/main/java/click/applemt/apmt/repository/userrepository/userRepository.java
+++ src/main/java/click/applemt/apmt/repository/userrepository/userRepository.java
@@ -4,15 +4,11 @@ import click.applemt.apmt.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-<<<<<<< HEAD
 import java.util.List;
 import java.util.Optional;
 
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByUid(String uid);
-=======
+
 @Repository
 public interface UserRepository extends JpaRepository<User,String> {
->>>>>>> parent of 559ef68 (++)
+    Optional<User> findByUid(String uid);
 }
