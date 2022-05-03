52bb8120f8132688136decc509eb2c898e001278
CokeBear <43844779+Kangchanghwan@users.noreply.github.com>
2022-05-03 17:39:27 +0900

    이상무님 병합

--- src/main/java/click/applemt/apmt/repository/reviewRepository/ReviewRepository.java
+++ src/main/java/click/applemt/apmt/repository/reviewRepository/ReviewRepository.java
@@ -9,10 +9,11 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 @Repository
-public interface ReviewRepository extends JpaRepository<Review, Long>{
+public interface ReviewRepository extends JpaRepository<Review, Long> {
 
     /**
      * TradeHistory ID를 입력하면 Review 리스트를 반환한다
+     *
      * @param tradeHistoryId TradeHistory ID 거래내역의 ID
      * @return 후기 내역
      */
@@ -22,8 +23,10 @@ public interface ReviewRepository extends JpaRepository<Review, Long>{
 
     /**
      * 판매자의 User ID를 입력하면 판매자의 거래후기 목록을 반환한다
+     *
      * @param uid 판매자의 User ID
      * @return 판매자의 거래 후기 내역
      */
     @Query("SELECT r FROM Review r WHERE r.sellerUid = :uid")
     List<Review> getReviewsBySellerUid(String uid);
+}
